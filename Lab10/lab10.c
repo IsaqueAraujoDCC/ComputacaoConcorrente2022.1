cc7e5de4c34f0b8cc67b68e14b29f342a5820fec
Isaque Araújo <isaquearaujo@dcc.ufrj.br>
2022-07-04 23:45:43 -0300

    Começando Implementação do Laboratório 10

---
+++ Lab10/lab10.c
@@ -0,0 +1,92 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+//gcc lab10.c -o lab10 -Wall -lpthread
+
+#define L 4 //numero de threads leitoras
+#define E 2 //numero de threads escritoras
+
+sem_t em_e, em_l, escr, leit; //semaforos
+int e=0, l=0; //globais
+//    printf("Leitora %d esta lendo\n", *id);
+//    printf("Escritora %d esta escrevendo\n", *id);
+//thread leitora
+void * leitor (void * arg) {
+  int *id = (int *) arg;
+  while(1) {
+    sem_wait(&leit);
+    sem_wait(&em_l); l++;
+    if(l == 1) {
+        //printf("Há escritoras esperando\n");
+        sem_wait(&escr);
+    }
+    sem_post(&em_l);
+    sem_post(&leit);
+    printf("Leitora %d esta lendo\n", *id);
+    sem_wait(&em_l); l--;
+    printf("Leitora %d terminou de ler\n", *id);
+    if(l == 0) {
+        sem_post(&escr);
+    }
+    sem_post(&em_l);
+    sleep(1);
+  } 
+  free(arg);
+  pthread_exit(NULL);
+}
+
+
+
+//thread leitora
+void * escritor (void * arg) {
+  int *id = (int *) arg;
+  while(1) {
+    sem_wait(&em_e); e++;
+    if(e == 1) {
+        //printf("Há leitoras esperando\n");
+        sem_wait(&leit);
+    }
+    sem_post(&em_e);
+    sem_wait(&escr);
+    printf("Escritora %d esta escrevendo\n", *id);
+    sem_post(&escr);
+    sem_wait(&em_e); e--;
+    printf("Escritora %d terminou de escrever\n", *id);
+    if(e == 0){ 
+        sem_post(&leit);
+    }
+    sem_post(&em_e);
+    sleep(1);
+  } 
+  free(arg);
+  pthread_exit(NULL);
+}
+
+//funcao principal
+int main(void) {
+  //identificadores das threads
+  pthread_t tid[L+E];
+  int id[L+E];
+
+  sem_init(&escr, 0, 1);
+  sem_init(&leit, 0, 1);
+  sem_init(&em_e, 0, 1);
+  sem_init(&em_l, 0, 1);
+
+  //cria as threads leitoras
+  for(int i=0; i<L; i++) {
+    id[i] = i+1;
+    if(pthread_create(&tid[i], NULL, leitor, (void *) &id[i])) exit(-1);
+  } 
+  
+  //cria as threads escritoras
+  for(int i=0; i<E; i++) {
+    id[i+L] = i+1;
+    if(pthread_create(&tid[i+L], NULL, escritor, (void *) &id[i+L])) exit(-1);
+  } 
+
+  pthread_exit(NULL);
+  return 0;
+}
